18b4d33e2fcd85754199f76d9fb3aa4613409d1f
Rigoberto Moreira <rigomoreirar@gmail.com>
2024-11-25 22:26:01 -0600

    Added validation for add method for avoiding duplicates, added a get_all_passwords method

--- password_store.py
+++ password_store.py
@@ -7,14 +7,27 @@ class PasswordStore:
         self.password = password
 
     def add_password(self, id, password):
+        """
+        Adds a new password with the given ID to the store.
+        """
         self.id = id
         self.password = password
+        
+        with open("passwords.txt", "r") as f:
+            lines = f.readlines()
+            for line in lines:
+                stored_id, stored_password = line.strip().split("|")
+                if stored_id == id:
+                    raise ValueError(f"ID '{id}' already exists in the store.")
+            
 
-        f = open("passwords.txt", "a")
-        f.write(f"{self.id}|{self.password}\n")
-        f.close()
+        with open("passwords.txt", "a") as f:
+            f.write(f"{self.id}|{self.password}\n")
 
     def update_password(self, id, password):
+        """
+        Updates the password for the given ID.
+        """
         self.id = id
         self.password = password
 
@@ -30,8 +43,11 @@ class PasswordStore:
                     f.write(line)
 
     def get_all_id(self):
-        f = open("passwords.txt", "r")
-        lines = f.readlines()
+        """
+        Retrieves a list of all IDs from the store.
+        """
+        with open("passwords.txt", "r") as f:
+            lines = f.readlines()
 
         id_list = []
 
@@ -42,8 +58,11 @@ class PasswordStore:
         return id_list
 
     def get_password(self, id):
-        f = open("passwords.txt", "r")
-        lines = f.readlines()
+        """
+        Retrieves the password for a specific ID.
+        """
+        with open("passwords.txt", "r") as f:
+            lines = f.readlines()
 
         password = ""
 
@@ -54,6 +73,29 @@ class PasswordStore:
 
         return password
 
+    def get_all_passwords(self):
+        """
+        Retrieves all IDs and passwords as a list of dictionaries.
+        """
+        with open("passwords.txt", "r") as f:
+            lines = f.readlines()
+
+        all_passwords = []
+
+        for line in lines:
+            stored_id, stored_password = line.strip().split("|")
+            all_passwords.append({"id": stored_id, "password": stored_password})
+
+        return all_passwords
+
 
 if __name__ == "__main__":
     test = PasswordStore()
+
+    # Example usage
+    test.add_password("email", "password123")
+    test.add_password("bank", "securepass456")
+
+    print("All IDs:", test.get_all_id())
+    print("Password for 'email':", test.get_password("email"))
+    print("All passwords:", test.get_all_passwords())
