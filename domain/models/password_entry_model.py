3bd875412b88d81e2f8a2e76ee44d21c21f35015
Rigoberto Moreira <rigomoreirar@gmail.com>
2024-12-04 23:39:20 -0600

    Added __repr__ to models, and url to argument model

--- domain/models/password_entry_model.py
+++ domain/models/password_entry_model.py
@@ -3,3 +3,6 @@ class PasswordEntryModel:
         self.username = username
         self.password = password
         self.domain = domain
+
+    def __repr__(self):
+        return f"PasswordEntryModel(username='{self.username}', password='{self.password}', domain='{self.domain}')"
