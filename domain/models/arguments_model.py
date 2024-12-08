1e9bfec4f941a3f74b24fd7e6726f660df58f0bd
Rigoberto Moreira <rigomoreirar@gmail.com>
2024-12-08 17:34:28 -0600

    Modified arguments model to handle password and extra_id in new command

--- domain/models/arguments_model.py
+++ domain/models/arguments_model.py
@@ -1,11 +1,21 @@
 class ArgumentsModel:
     def __init__(self, help=False, new=None, get=None, update=None, upload=False, error=None):
         self.help = help
-        self.new = new or {"username": None,
-                           "type": None, "seed": None, "domain": None}
+        self.new = new or {
+            "username": None,
+            "type": None,
+            "seed": None,
+            "domain": None,
+            "extra_id": None,  
+            "password": None   
+        }
         self.get = get or {"username": None, "all": None, "domain": None}
-        self.update = update or {"username": None,
-                                 "type": None, "seed": None, "domain": None}
+        self.update = update or {
+            "username": None,
+            "type": None,
+            "seed": None,
+            "domain": None
+        }
         self.upload = upload
         self.error = error
 
