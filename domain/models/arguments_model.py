f10eab8375d734b819d950b7a64741fc61c2b1f1
Rigoberto Moreira <rigomoreirar@gmail.com>
2024-12-08 18:40:23 -0600

    Added new command for deleting pst entries

--- domain/models/arguments_model.py
+++ domain/models/arguments_model.py
@@ -1,5 +1,5 @@
 class ArgumentsModel:
-    def __init__(self, help=False, new=None, get=None, update=None, upload=False, error=None):
+    def __init__(self, help=False, new=None, get=None, update=None, upload=False, error=None, delete=None):
         self.help = help
         self.new = new or {
             "username": None,
@@ -8,7 +8,11 @@ class ArgumentsModel:
             "domain": None,
             "password": None   
         }
-        self.get = get or {"username": None, "all": None, "domain": None}
+        self.get = get or {
+            "username": None, 
+            "all": None, 
+            "domain": None
+            }
         self.update = update or {
             "username": None,
             "type": None,
@@ -16,8 +20,13 @@ class ArgumentsModel:
             "domain": None
         }
         self.upload = upload
+        self.delete = delete or {
+            "username": None, 
+            "domain": None
+            }
         self.error = error
+        
 
     def __repr__(self):
         return (f"ArgumentsModel(help={self.help}, new={self.new}, get={self.get}, "
-                f"update={self.update}, upload={self.upload}, error={self.error})")
+                f"update={self.update}, upload={self.upload}, error={self.error}, delete={self.delete})")
